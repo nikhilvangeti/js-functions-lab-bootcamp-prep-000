d404edcaddd61e30b871cb45cdf38557937b4396
nikhil vangeti <vangeti.nikhil630@gmail.com>
2018-01-21 01:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays()
+{
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name)
+{
+  var fone='Happy holidays, ${name}!'
+  return fone
+}
+
+function happyHoidayTo(name,holiday)
+{
+  var ftwo='Happy ${holiday}, ${name}!'
+  return ftwo
+}
+
+function holidayCountdown(days, holiday)
+{
+  var fthree="It's ${days} until ${holiday}!"
+  return fthree
+}
+
