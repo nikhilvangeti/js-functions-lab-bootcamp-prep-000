430f268908d687c83745e5de02bd038ef2adacd7
nikhil vangeti <vangeti.nikhil630@gmail.com>
2018-01-21 02:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ function happyHolidaysTo(name)
 {
   
   var message= 'Happy holidays, ${name}!'
+  console.log(message)
   return message
 }
 
 function happyHolidayTo(name, holiday)
 {
   var message1='Happy ${holiday}, ${name}!'
+  console.log(message1)
   return message1
 }
 
 function holidayCountdown(days, holiday)
 {
   var message3="It's ${days} days until ${holiday}!"
+  console.log(message3)
   return message3
 }
 
