df86ce4ecc6c4e7aa579fb15c7b466d84579ce84
nikhil vangeti <vangeti.nikhil630@gmail.com>
2018-01-21 01:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name)
   return 'Happy holidays, ${name}!'
 }
 
-function happyHoidayTo(name, holiday)
+function happyHolidayTo(name, holiday)
 {
   return 'Happy ${holiday}, ${name}!'
 }
